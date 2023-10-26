2cff83c3dd9fee33e88c6937d2af65b6c5a5f0ef
rainzoneg <samuelnathaniel1234@gmail.com>
2023-10-26 11:10:01 +0700

    Seperation of face and license plate detection projects

---
+++ README.md
@@ -0,0 +1,16 @@
+# YOLOV8 WEBCAM DETECTION
+
+## Under-Construction.
+
+Run project in a virtual environment (venv\Scripts\Activate in directory terminal)
+
+python -m track_yolo8 to run the main project
+
+pip install -r requirements.txt to install requirements
+
+pip freeze > requirements.txt to update requirements
+
+pip install -U [MODULE NAME] --user to update packages and resolve administrative restrictions if needed
+
+pip uninstall [MODULE NAME] old bad versions if needed
+
--- license_track_yolo8.py
+++ license_track_yolo8.py
@@ -5,13 +5,12 @@ import os
 
 WEBCAM_URL = "placeholder";
 
-##Load the Face Recognition YOLO model & improve memory efficiency
-face_model = YOLO("models/yolov8n-face.pt")
-face_model.fuse()
+##Load the License Plate Recognition YOLO model & improve memory efficiency
+license_plate_model = YOLO("models/license_plate_detector.pt")
+license_plate_model.fuse()
 
-
-#Load our webcam
-cam = cv2.VideoCapture(0)
+##Load our sample video
+cam = cv2.VideoCapture("sample/videos/sample.mp4")
 
 ##Test if cam failed to open
 while not cam.isOpened():
@@ -28,8 +27,7 @@ while cam.isOpened():
     ##If frame retrieval succesful, process the frame and perform inference on the frame
     if success:
         ##Perform YOLOv8 inference -> tracking mode, persist id/box between cam frames
-        # trackResults = face_model.track(image, persist=True)
-        trackResults = face_model.track(image, persist=True)
+        trackResults = license_plate_model.track(image, persist=True)
 
         ##Plot the inference results, to annotate with rect box, text
         annotated_frame = trackResults[0].plot()
@@ -44,17 +42,17 @@ while cam.isOpened():
         else:
             trackIds = trackResults[0].boxes.id.cpu().numpy().astype(int)
 
-        ##Retrieve properties about inferred result, check if it detects a face (face class stored in cls variable as 0)
+        ##Retrieve properties about inferred result, check if it detects a license plate (licenes plate class stored in cls variable as 0)
         for box, track_id in zip(track_boxes, trackIds):
             cls = int(box.cls[0])
-            ##If a person's face is detected in the detection box,
+            ##If a license plate is detected in the detection box,
             if cls == 0:
-                if count % 10 == 0:
+                if count % 5 == 0:
                     ##Then iterate for each existing instances of boxes / people:
                     for trackId in trackIds:
                         ##Save the annotated frame as an image file in a local directory.
                         current_time = time.strftime("%H-%M", time.localtime())
-                        filename = "person_" + str(trackId) + "_" + current_time + ".jpg"
+                        filename = "licenseplate_" + str(trackId) + "_" + current_time + ".jpg"
                         absolute_path = os.path.join(os.getcwd(), 'results', filename)
                         print(absolute_path)
                         cv2.imwrite(absolute_path, annotated_frame)
@@ -63,8 +61,9 @@ while cam.isOpened():
 
         ##Show the video frame in screen for demonstration purposes
 
-        ##Make window resizable
+        ##Make window resizable, set default size to w800 x h600
         cv2.namedWindow("Real Time Tracking, Q to quit", cv2.WINDOW_NORMAL)
+        cv2.resizeWindow("Real Time Tracking, Q to quit", 800, 600)
         cv2.imshow("Real Time Tracking, Q to quit", annotated_frame)
         if cv2.waitKey(1) & 0xFF == ord("q"):
             break
